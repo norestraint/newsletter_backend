d13420e3de61fbb64613b60a0450935af2a1eff7
norestraint <norestraint@protonmail.com>
2023-07-04 00:28:01 -0300

    fix: remove underscore to pass the tests
    I added the underscore to stop the 'dead_code' warning, but that breaks the serializer so...

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,8 +2,8 @@ use actix_web::{web, HttpResponse};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    _email: String,
-    _name: String,
+    email: String,
+    name: String,
 }
 
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
