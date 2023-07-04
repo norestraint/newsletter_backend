f4241b760405d61eee5fdad445e9be553f5ae65b
norestraint <norestraint@protonmail.com>
2023-07-04 18:15:21 -0300

    feat: fix db connection problem by using PgPool instead of PgConnection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
@@ -7,8 +8,11 @@ use zero2prod::startup::run;
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration.
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
