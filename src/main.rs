19b3bbf11fb2792fcab21f0c6f831e272ad403ce
norestraint <norestraint@protonmail.com>
2023-07-03 21:55:46 -0300

    refactor: organize code into a file for each route + one for app startup and config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Launching app at: 127.0.0.1:{}", &port);
     run(listener)?.await
 }
