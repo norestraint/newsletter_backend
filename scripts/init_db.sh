a000b05f899b5d18c786c18e43f73699b00aee9a
norestraint <norestraint@protonmail.com>
2023-07-12 14:04:13 -0300

    fix: fix db starter script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,7 +15,7 @@ echo >&2 "to install it."
 exit 1
 fi
 
-DB_USER=${POSTGRES_USER:=postgres}
+DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
