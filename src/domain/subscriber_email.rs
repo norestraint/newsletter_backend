ef42ec1c728b80c34f6de16e7a162877419265fe
norestraint <norestraint@protonmail.com>
2023-07-20 16:14:56 -0300

    tests: add basic tests for subscribers email

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -14,3 +14,25 @@ impl AsRef<str> for SubscriberEmail {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::SubscriberEmail;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert!(SubscriberEmail::parse(email).is_err());
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert!(SubscriberEmail::parse(email).is_err());
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert!(SubscriberEmail::parse(email).is_err());
+    }
+}
