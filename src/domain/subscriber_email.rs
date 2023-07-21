006ed17f7819812a0eade2125545ef5c70e8fa00
norestraint <norestraint@protonmail.com>
2023-07-21 14:12:33 -0300

    test: use quickcheck to generate valid email

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -25,10 +25,19 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        assert!(SubscriberEmail::parse(email).is_ok())
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 
     #[test]
